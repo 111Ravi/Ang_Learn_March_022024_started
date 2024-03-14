e78673f4c2915aee4a5d451451ab84dbb33dcf0c
111Ravi <ravi.soft111@gmail.com>
2024-03-14 23:24:38 +0530

    changes start assignent2_1

--- Angulerapp/src/app/employee/employee.component.ts
+++ Angulerapp/src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { employee } from './employee';
 
 @Component({
   selector: 'app-employee',
---
+++ Angulerapp/src/app/employee/employee.ts
@@ -0,0 +1,9 @@
+export interface employee
+{
+    Employee_ID: number,
+    First_Name: string,
+    Last_Name:string,
+    Salary: number,
+    DOB:Date,
+    Email:string
+}
