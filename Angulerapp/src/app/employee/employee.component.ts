e91e4b979fbe70a979b7dbe960e400691a14a7e8
111Ravi <ravi.soft111@gmail.com>
2024-03-15 23:00:24 +0530

    changesregarding edit

--- Angulerapp/src/app/employee/employee.component.ts
+++ Angulerapp/src/app/employee/employee.component.ts
@@ -7,5 +7,13 @@ import { employee } from './employee';
   styleUrls: ['./employee.component.css']
 })
 export class EmployeeComponent {
-
+employee_id=1;
+first_name=`Ravi`;
+last_name="Singh";
+salary=50000;
+dob="5/07/1988";
+email="ravi.soft111@gmail.com";
+handleClick(){
+  console.log('Button Clicked')
+}
 }
