f10a2259327e1fd27f3ebf7630d75cc70b00460a
111Ravi <ravi.soft111@gmail.com>
2024-03-18 23:53:51 +0530

    added component

---
+++ Fullstackapp/src/app/Header/Header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-header',
+    templateUrl:'./header.component.html',
+    styleUrls:['./header.component.css',]
+    })
+
+export class HeaderComponent{
+
+
+    title='learning with Devloper funnel.'
+    imgurl='https://b.zmtcdn.com/data/pictures/4/307374/b2b03be3aba61b0f173aa23e1abdb42b.jpg'
+}
