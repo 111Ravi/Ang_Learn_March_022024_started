bf38b7701cceba9f0f7b6fe324ce9743810f4bf2
111Ravi <ravi.soft111@gmail.com>
2024-03-29 22:41:59 +0530

    templet form

--- templateForm/src/app/app-routing.module.ts
+++ templateForm/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from './forms/forms.component';
+import { AfterSubmitComponent } from './after-submit/after-submit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'after',component: AfterSubmitComponent},
+  {path:'home',component: FormsComponent},
+  {path:'',component: FormsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- templateForm/src/app/app.module.ts
+++ templateForm/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { AfterSubmitComponent } from './after-submit/after-submit.component';
+import { FormService } from './forms/forms.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    AfterSubmitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    FormService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
